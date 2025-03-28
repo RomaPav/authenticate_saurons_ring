0d08bbd1e8f38403932c80bcbd1293b5e882f873
RomaPav <romatv321@gmail.com>
2025-03-28 20:17:36 +0200

    changed db url

--- database/database.py
+++ database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://romanus:L3c4tDtn5ztwn20QTj28S4o0VPfgPCBY@dpg-csjqad9u0jms73b4ds9g-a.oregon-postgres.render.com/marketplace_shop"
+# SQLALCHEMY_DATABASE_URL = "postgresql://romanus:L3c4tDtn5ztwn20QTj28S4o0VPfgPCBY@dpg-csjqad9u0jms73b4ds9g-a.oregon-postgres.render.com/marketplace_shop"
+SQLALCHEMY_DATABASE_URL = "postgresql://sauron_marketplace_user:LfIVmnkTClxcKQLvOCHjXQNqS6t8P0BH@dpg-cvjed1adbo4c738scn2g-a.oregon-postgres.render.com/sauron_marketplace"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
